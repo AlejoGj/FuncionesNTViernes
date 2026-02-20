6a01f14f69f20708177b74e977c626c564396eab
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-02-20 10:52:58 -0500

    feat se crea funcion generadora de listas

---
+++ bicicletas.py
@@ -0,0 +1,8 @@
+#Crear una lista de 30 estudiantes
+
+
+#crear una lista de 20 calificaciones de eficiencia
+#crear una lista de 20 calificaciones de apariencia
+#crear una lista de 20 calificaciones de estabilidad
+#calcular el promedio de las notas
+#evaluar la bicicleta ganadora
